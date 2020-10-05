c0ebe1904364baccc134606e6929ea50775e448f
utkarshmega <utkarsheduc@gmail.com>
2020-10-05 14:38:16 +0530

    First Commit

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,23 +1,22 @@
 package com.capgemini.tictactoe;
+
 import java.util.*;
 
 public class TicTacToeGame {
-	
+
 	char board[];
-	void board()
-	{
+
+	void board() {
 		board = new char[10];
 		Arrays.fill(board, ' ');
-		for(char c: board)
-			System.out.println("b"+c);
+		for (char c : board)
+			System.out.println("b" + c);
 	}
 
 	public static void main(String[] args) {
-		
+
 		TicTacToeGame obj = new TicTacToeGame();
 		obj.board();
-		
-		
 
 	}
 
