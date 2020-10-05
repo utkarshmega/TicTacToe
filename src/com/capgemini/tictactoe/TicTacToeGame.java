09b2f1765cb9df2b650071d70d590880bd9add38
utkarshmega <utkarsheduc@gmail.com>
2020-10-05 14:46:32 +0530

    UC2 done

---
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -0,0 +1,41 @@
+package com.capgemini.tictactoe;
+
+import java.util.*;
+
+public class TicTacToeGame {
+
+	private char board[];
+	private char player;
+	private char computer;
+
+	/**
+	 * Function to create board
+	 * 
+	 */
+	private char[] createBoard() {
+		board = new char[10];
+		Arrays.fill(board, ' ');
+		return board;
+	}
+
+	/**
+	 * Assigning letter to the player as per their choice
+	 */
+	private void playerLetter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter X or O as per your choice");
+		player = sc.next().charAt(0);
+		if (player == 'x')
+			computer = 'o';
+		else
+			computer = 'x';
+		sc.close();
+	}
+
+	public static void main(String[] args) {
+
+		TicTacToeGame tic_tac_toe = new TicTacToeGame();
+		char board[] = tic_tac_toe.createBoard();
+		tic_tac_toe.playerLetter();
+	}
+}
