471229a1addc3d1bd5908178b30b1ca066c09801
utkarshmega <utkarsheduc@gmail.com>
2020-10-05 14:41:39 +0530

    package name modified

--- src/com/capgemini/tictactoe/TicTacToeGame.java
+++ src/com/capgemini/tictactoe/TicTacToeGame.java
@@ -1,22 +1,23 @@
-package ticTacToe;
+package com.capgemini.tictactoe;
+
 import java.util.*;
 
 public class TicTacToeGame {
-	
+
 	private char board[];
-	
-	/** Function to create board
+
+	/**
+	 * Function to create board
 	 * 
 	 */
-	private void createBoard()
-	{
+	private void createBoard() {
 		board = new char[10];
 		Arrays.fill(board, ' ');
 	}
 
 	public static void main(String[] args) {
-		
+
 		TicTacToeGame tic_tac_toe = new TicTacToeGame();
-		tic_tac_toe.createBoard(); 
+		tic_tac_toe.createBoard();
 	}
 }
