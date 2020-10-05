2970d5ce47dfc541e687923233c96f4b8fc82cd8
utkarshmega <utkarsheduc@gmail.com>
2020-10-05 09:59:04 +0530

    UC2 modified & done

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -1,30 +1,31 @@
 package ticTacToe;
+
 import java.util.*;
 
 public class TicTacToeGame {
-	
+
 	private char board[];
 	private char player1;
 	private char computer;
-	/** Function to create board
+
+	/**
+	 * Function to create board
 	 * 
 	 */
-	private char[] createBoard()
-	{
+	private char[] createBoard() {
 		board = new char[10];
 		Arrays.fill(board, ' ');
 		return board;
 	}
+
 	/**
-	 * Assigning letter to the player
-	 * as per their choice
+	 * Assigning letter to the player as per their choice
 	 */
-	private void playerLetter()
-	{
+	private void playerLetter() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter X or O as per your choice");
 		player1 = sc.next().charAt(0);
-		if(player1=='x')
+		if (player1 == 'x')
 			computer = 'o';
 		else
 			computer = 'x';
@@ -32,7 +33,7 @@ public class TicTacToeGame {
 	}
 
 	public static void main(String[] args) {
-		
+
 		TicTacToeGame tic_tac_toe = new TicTacToeGame();
 		char board[] = tic_tac_toe.createBoard();
 		tic_tac_toe.playerLetter();
