f3e0e5ada6c3abea30cb5bf8ddd5669eae020f41
utkarshmega <utkarsheduc@gmail.com>
2020-10-05 09:05:23 +0530

    UC1 done

--- src/ticTacToe/TicTacToeGame.java
+++ src/ticTacToe/TicTacToeGame.java
@@ -8,16 +8,12 @@ public class TicTacToeGame {
 	{
 		board = new char[10];
 		Arrays.fill(board, ' ');
-		for(char c: board)
-			System.out.println("b"+c);
 	}
 
 	public static void main(String[] args) {
 		
 		TicTacToeGame obj = new TicTacToeGame();
-		obj.board();
-		
-		
+		obj.board(); 
 
 	}
 
